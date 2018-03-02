5bd62a0e78497d59ecc4e4f125f05fdfc8a19ce5
Lorna Jane Mitchell <lorna@lornajane.net>
2018-03-02 14:45:38 +0000

    Accept new records via the microservice

--- js/write-plan/index.js
+++ js/write-plan/index.js
@@ -5,13 +5,27 @@ function main(params) {
     return new Promise(function(resolve, reject) {
         console.log("make plan");
         var url = params.postgres_url;
-        db = pgp(params.postgres_url, []);
+        db = pgp(url, []);
 
         var incoming = new Buffer(params.__ow_body, 'base64').toString('utf-8');
-        console.log(incoming);
-        resolve({body: "good enough for now"});
+        var decoded = JSON.parse(incoming);
+        var location = decoded.location;
+        var travel_date = decoded.travel_date
 
-        pgp.end();
+        db.one("INSERT INTO plans (location, travel_date) VALUES  ($1, $2) RETURNING plan_id",
+            [location, travel_date])
+            .then(function(data) {
+                console.log("created plan with ID " + data.plan_id);
+                pgp.end();
+                var redirect_to = params.base_url + "/" + data.plan_id;
+                console.log ("Redirecting to: " + redirect_to);
+                resolve({headers: {"Location": redirect_to}, statusCode: 303})
+            })
+            .catch(function(error) {
+                console.log(error);
+                pgp.end();
+                reject({body: "An error occurred", statusCode: 400});
+            });
     });
 }
 
