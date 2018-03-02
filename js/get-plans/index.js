a63576a04b6686904fb3ce248fdae776fb3c432f
Lorna Jane Mitchell <lorna@lornajane.net>
2018-03-02 14:44:55 +0000

    Support returning individual records as well as a collection

--- js/get-plans/index.js
+++ js/get-plans/index.js
@@ -5,19 +5,39 @@ function main(params) {
     return new Promise(function(resolve, reject) {
         console.log("get plans");
         var url = params.postgres_url;
-        db = pgp(params.postgres_url, []);
+        db = pgp(url, []);
 
-        db.any("SELECT * FROM plans", [true])
-            .then(function(data) {
-                console.log(JSON.stringify(data));
-                pgp.end();
-                resolve({body: {plans: data}, statusCode: 200});
-            })
-            .catch(function(error) {
-                console.log(error);
-                pgp.end();
-                reject({body: "An error occurred", statusCode: 400});
-            });
+        // one record or many?
+        var plan_id;
+        if(params.__ow_path) {
+            var path_data = params.__ow_path.match(/^\/([0-9]+)/);
+            plan_id = path_data[1];
+        }
+
+        if(plan_id) {
+            db.any("SELECT * FROM plans WHERE plan_id = $1", [plan_id])
+                .then(function(data) {
+                    pgp.end();
+                    resolve({body: {plans: data}, statusCode: 200});
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    pgp.end();
+                    reject({body: "An error occurred", statusCode: 400});
+                });
+
+        } else {
+            db.any("SELECT * FROM plans", [true])
+                .then(function(data) {
+                    pgp.end();
+                    resolve({body: {plans: data}, statusCode: 200});
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    pgp.end();
+                    reject({body: "An error occurred", statusCode: 400});
+                });
+        }
     });
 }
 
