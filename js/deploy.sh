56d17d48d1692d21b03a8215ed44900298d1144c
Lorna Jane Mitchell <lorna@lornajane.net>
2018-05-30 11:40:57 +0100

    Update to ibmcloud command name

--- js/deploy.sh
+++ js/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-bx wsk package update plans-api
-bx wsk service bind compose-for-postgresql plans-api
+ibmcloud wsk package update plans-api
+ibmcloud wsk service bind compose-for-postgresql plans-api
 
 cd get-plans
 zip -rq get-plans.zip index.js node_modules
-bx wsk action update --kind nodejs:6 --web raw plans-api/get-plans get-plans.zip
+ibmcloud wsk action update --kind nodejs:8 --web raw plans-api/get-plans get-plans.zip
 # set the API URL
-bx wsk api create /plans GET plans-api/get-plans --response-type http
+ibmcloud wsk api create /plans GET plans-api/get-plans --response-type http
 cd ..
 
 cd write-plan
 zip -rq write-plan.zip index.js node_modules
-bx wsk action update --kind nodejs:6 --web raw plans-api/write-plan write-plan.zip
-bx wsk api create /plans POST plans-api/write-plan --response-type http
+ibmcloud wsk action update --kind nodejs:6 --web raw plans-api/write-plan write-plan.zip
+ibmcloud wsk api create /plans POST plans-api/write-plan --response-type http
 cd ..
 
 
